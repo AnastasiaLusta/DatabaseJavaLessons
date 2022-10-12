377919085976a75c3561b8e3d847b29643667c53
AnastasiaLusta <itssialusta@gmail.com>
2022-10-12 21:17:34 +0300

    feat: validation for registration of user

--- src/main/java/step/learning/Main.java
+++ src/main/java/step/learning/Main.java
@@ -5,8 +5,8 @@ import com.google.inject.Guice;
 public class Main {
     public static void main(String[] args) {
         var configModule = new ConfigModule();
-//        Guice.createInjector(configModule).getInstance(AppUser.class).run();
-        Guice.createInjector(configModule).getInstance(App.class).run();
+        Guice.createInjector(configModule).getInstance(AppUser.class).run();
+//        Guice.createInjector(configModule).getInstance(App.class).run();
         configModule.close();
     }
 }
